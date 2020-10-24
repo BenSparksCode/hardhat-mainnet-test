d31735ed16c2e6b07b93e0cf1a90288662317444
Ben Sparks <bensparks0@gmail.com>
2020-10-24 17:41:10 +0200

    Added task to print Vitalik's ETH balance on mainnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -20,6 +20,26 @@ task("get_current_block", "Prints the current block", async () => {
   console.log("CURRENT BLOCK: ", block)
 });
 
+task("get_vb_balance", "Gets Vitalik's ETH account balance", async () => {
+
+  const res = await hre.network.provider.request({
+    method: "eth_getBalance",
+    params: ["0xab5801a7d398351b8be11c439e05c5b3259aec9b", 'latest']}
+  )
+
+  console.log("Result: ", parseInt(res)*10**-18)
+});
+
+task("impersonate_vb", "Impersonates Vitalik and prints his account balance", async () => {
+
+  const res = await hre.network.provider.request({
+    method: "hardhat_impersonateAccount",
+    params: ["0xab5801a7d398351b8be11c439e05c5b3259aec9b"]}
+  )
+
+  console.log("Result: ", res)
+});
+
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
